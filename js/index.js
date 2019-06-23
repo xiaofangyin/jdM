08cb28aad163bb3831b47e6e560f0bfd8b6ebfef
enzo <xiaofangyinwork@163.com>
2019-06-23 08:45:01 +0800

    轮播图添加动画

--- js/index.js
+++ js/index.js
@@ -23,12 +23,26 @@ var search = function () {
             opacity = 0.85;
         }
         search.style.backgroundColor = "rgba(201,21,35," + opacity + ")";
-        console.log(scrollTop, opacity);
     };
 };
 
 var banner = function () {
+    var banner = document.querySelector(".jd_banner");
+    var width = banner.offsetWidth;
+    var imageBox = banner.querySelector("ul:first-child");
+    var indicator = banner.querySelector("ul:last-child");
 
+    var index = 1;
+    var timmer = setInterval(function () {
+        index++;
+        /*加过度*/
+        imageBox.style.transition = "all 0.2s";
+        imageBox.style.webkitTransform = "all 0.2s";
+
+        /*做动画*/
+        imageBox.style.transform = "translateX(" + (-width * index) + "px)";
+        imageBox.style.webkitTransform = "translateX(" + (-width * index) + "px)";
+    }, 3000);
 };
 
 var countDown = function () {
